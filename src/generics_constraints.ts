9b7acc780a74d945b9440b8c44b85d995c27e8f6
lkmdev <lkmdev01@gmail.com>
2023-04-02 20:30:02 -0300

    Feito!

--- src/generics_constraints.ts
+++ src/generics_constraints.ts
@@ -1,5 +1,31 @@
-const display = <T, U>(p1:T,p2:U): void =>{
-
+class Person{
+  constructor(
+    public name: string,
+    public age: number,
+  ){}
 }
 
-display<string, number>('name',123)
+const display = <T extends Person, U>(person:T,p2:U): void =>{
+  console.log(person.name)
+}
+
+const display2 = <T extends Person, U extends T>(person:T,p2:U): void =>{
+  console.log(person.name)
+}
+
+const display3 = <T extends Person, U extends string|number>(person:T,p2:U): void =>{
+  console.log(person.name)
+}
+
+const p1 = new Person("Lukas", 20)
+display3<Person,string>(p1,'123')
+
+class User extends Person{}
+const p2 = new User("Lukas", 20)
+display(p2,123)
+
+
+// Não funciona porque o 'test' não é do tipo
+// class Test{}
+// const test = new Test()
+// display(test,123)
